70e4a3ecc70d2c6d3ff2fa02e79f086ee3839344
Gabriel <83117210+Princegabz@users.noreply.github.com>
2023-11-13 21:35:58 +0200

    Initial commit

---
+++ app/src/main/java/com/n3t0l0b0/blogspot/mpc/view/searchbarcompose/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.n3t0l0b0.blogspot.mpc.view.searchbarcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF8BC34A)
+val PurpleGrey80 = Color(0xFF858B7D)
+val Pink80 = Color(0xFFC3FF7E)
+
+val Purple40 = Color(0xFF38883B)
+val PurpleGrey40 = Color(0xFF1F7923)
+val Pink40 = Color(0xFF508152)
